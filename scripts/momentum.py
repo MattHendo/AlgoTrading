6c6f01021d0a766a8804a28df1401a26914ac568
Matthew Nighting-Henderson <matt.henderson2004@knights.ucf.edu>
2024-10-26 09:08:56 -0400

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	config.py

--- scripts/momentum.py
+++ scripts/momentum.py
@@ -1,1 +1,53 @@
 #momentum strategy
+#this is an example from the youtube video of a momentum strategy
+from config import ALPACA_CONFIG
+from lumibot.brokers import Alpaca
+from lumibot.strategies import Strategy
+from lumibot.traders import Trader
+
+API_KEY="PK30NYXLXKB85V6L8BRR"
+SECRET_KEY ="3MgoJh48RZodsWH7b3PEd81uvBWjhj1ifxZz25zc"
+ENDPOINT=  "https://paper-api.alpaca.markets/v2"
+
+
+class SwingHigh(Strategy):
+    data = []
+    order_number = 0
+    def initialize(self):
+        self.sleeptime = "10S"
+
+
+    def on_trading_iteration(self):
+        symbol ="AAPL"
+        entry_price = self.get_last_price(symbol)
+        self.log_message(f"Position: {self.get_position(symbol)}")
+        self.data.append(self.get_last_price(symbol))
+
+        if len(self.data) > 3:
+            temp = self.data[-3:]
+            if temp[-1] > temp[1] > temp[0]:
+                self.log_message(f"Last 3 prints: {temp}")
+                order = self.create_order(symbol, quantity = 10, side = "buy")
+                self.submit_order(order)
+                self.order_number += 1
+                if self.order_number == 1:
+                    self.log_message(f"Entry price: {temp[-1]}")
+                    entry_price = temp[-1] # filled price
+            if self.get_position(symbol) and self.data[-1] < entry_price * .995:
+                self.sell_all()
+                self.order_number = 0
+            elif self.get_position(symbol) and self.data[-1] >= entry_price * 1.015:
+                self.sell_all()
+                self.order_number = 0
+
+
+    def before_market_closes(self):
+        self.sell_all()
+
+
+if __name__ == "__main__":
+    broker = Alpaca(ALPACA_CONFIG)
+    strategy = SwingHigh(broker=broker)
+    trader = Trader()
+    trader.add_strategy(strategy)
+    trader.run_all()
