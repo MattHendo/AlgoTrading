fca8a05cab7636de30251fa1dab31c7b1ebbc448
Matthew Nighting-Henderson <matt.henderson2004@knights.ucf.edu>
2025-01-12 18:36:43 -0500

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	config.py

--- scripts/USEPairsTradingStrat.py
+++ scripts/USEPairsTradingStrat.py
@@ -6,8 +6,8 @@ from lumibot.traders import Trader
 from config import ALPACA_CONFIG
 import pandas as pd
 
-API_KEY = "PK2BPY2BJH8UJ9CB3PJB"
-SECRET_KEY = "WEiBPpmuS1hT7BtGFc7FQSm5YMcOLPA15KPTbgcq"
+API_KEY="PKK0JP0AA7PS0M1PS437"
+SECRET_KEY ="y8XKrt8IazYOyGjMPWY6tZmXkWZyfZUnG2JAxwPI"
 
 ALPACA_CONFIG = {
     "API_KEY": API_KEY,
@@ -70,7 +70,7 @@ trader = Trader()
 broker = Alpaca(ALPACA_CONFIG)
 
 # Set up the strategy with parameters
-strategy = TrendFollowingStrategy(
+strategy = PairsTrading(
     broker=broker,
     parameters={
         "symbol": "SPY",
